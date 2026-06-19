6e656e7758865637f7a943564c776aacd661617c
lollibooksteamtay-ux <lollibooksteamtay@gmail.com>
2026-06-19 11:55:48 +0700

    Fix dashboard: force-dynamic để hiện ứng viên mới realtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getDb } from '@/lib/db';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Dashboard() {
   const db = await getDb();
 
